f048148bba54b6de9f26b495eed966403c074f4e
Shyamcode01 <shyamyadav121240@gmail.com>
2024-03-26 12:26:52 +0530

    first commit code

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let navsectin=document.querySelector('.navSection');
+let nav="nav2.html";
+ 
+
+fetch("service.html")
+.then(function(response){
+   console.log(response)
+})
+ 
+ 
+ 
+   
+ 
+ 
+
+ 
